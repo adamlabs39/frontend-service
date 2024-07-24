c7fd43529c7edd8993be1c21d4e9ce01aafc52e0
fahmi-nugroho <fahminugroho23@gmail.com>
2024-07-24 16:14:44 +0700

    fix: add prime vue library

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,26 @@ import App from './App.vue'
 import router from './router'
 import PhosphorIcons from "@phosphor-icons/vue"
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(PhosphorIcons)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark',
+      cssLayer: {
+        name: "primevue",
+        order: "tailwind-base, primevue, tailwind-utilities",
+      },
+    },
+  },
+});
 
 app.mount('#app')
