2b33b3e60b72083fb2bda399db181a1b53be7226
adamrasyid01 <adamrasyid01@gmail.com>
2024-08-09 14:10:25 +0700

    Memperbaiki Size Button Halaman Profil Faskes

--- src/views/SettingPage/Template/ProfilFaskes/LogoWarna.vue
+++ src/views/SettingPage/Template/ProfilFaskes/LogoWarna.vue
@@ -46,7 +46,7 @@ import CustomButton from '@/components/Base/CustomButton.vue';
 				</ul>
 			</div>
 			<div>
-				<CustomButton icon="PhCheck"/>
+				<CustomButton icon="PhCheck" class="w-[100px] h-14"/>
 			</div>
 
 		</div>
