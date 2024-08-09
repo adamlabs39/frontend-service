65fc871052cfd01e2d5a541b4866e07c0daeb9b8
adamrasyid01 <adamrasyid01@gmail.com>
2024-08-09 17:55:17 +0700

    Push Dashboard

---
+++ src/views/Dashboard/CardDashboard.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+const props = defineProps({
+	iconKegiatan: {
+		type: String,
+		default:""
+	},
+	namaKegiatan: {
+		type: String,
+		default:"Tidak Tau"
+	},
+	data: {
+		type: String,
+		default:"HALO"
+	},
+	satuanData: {
+		type: String,
+		default:"KM"
+	},
+	percentage: {
+		type: String,
+		default:"10%"
+	},
+	arrowIcon: {
+		type: String,
+		default:""
+	}
+})
+</script>
+
+<template>
+	<div class="w-auto p-5 bg-white rounded-lg shadow-md">
+		<!-- Header Section -->
+		<div class="flex items-center gap-2.5 mb-4">
+			<div class="w-10 h-10 text-white bg-teal-400 rounded-full">
+				<img :src="props.iconKegiatan" alt="Icon Kegiatan" class="w-full h-full rounded-full">
+			</div>
+			<div class="font-semibold text-black text-SM">{{ props.namaKegiatan }}</div>
+		</div>
+
+		<!-- Main Content -->
+		<div class="">
+			<div class="flex flex-col justify-end">
+				<div class="font-bold text-black text-[40px]">{{ props.data }}</div>
+				<div class="text-gray-400 text-normal">{{props.satuanData}}</div>
+			</div>
+			<div class="flex justify-end -mt-6">
+				<div>
+					<img :src="props.arrowIcon" alt="Icon Kegiatan" class="w-full h-full rounded-full">
+				</div>
+				<div class="text-normal text-success-300">{{props.percentage}}</div>
+			</div>
+
+		</div>
+	</div>
+</template>
--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -1,38 +1,35 @@
 <script setup lang="ts">
+import CardDashboard from './CardDashboard.vue'
 
 </script>
 
 
 
 <template>
-  <div class="w-auto p-5 bg-white rounded-lg shadow-md">
-    <!-- Header Section -->
-    <div class="flex items-center gap-2.5 mb-4">
-      <div class="w-10 h-10 text-white bg-teal-400 rounded-full">
+  <div class="flex flex-col gap-5">
+    <div class="flex items-center gap-2.5 p-5 bg-white rounded-lg">
+      <div class="p-2 text-white bg-teal-400 rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M5.121 19a4.5 4.5 0 006.379 0M9 12.75A6 6 0 1121 12.75M9 12.75a3 3 0 100-6h.75" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2l4-4m2 1.5L9 21m0-7h3" />
         </svg>
       </div>
-      <div class="font-semibold text-black text-SM">Total Kunjungan</div>
+      <div class="ml-2 font-semibold text-teal-500 text-subTitle">Dashboard</div>
     </div>
-
-    <!-- Main Content -->
-    <div class="">
-      <div class="flex flex-col justify-end">
-        <div class="font-bold text-black text-[40px] ">125</div>
-        <div class="text-gray-400 text-normal">Pasien</div>
+    
+    <div class="flex gap-2.5">
+      <div class="w-1/4">
+        <CardDashboard />
       </div>
-      <div class="flex justify-end -mt-6">
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-          </svg>
-        </div>
-        <div class="text-normal text-success-300">20%</div>
+      <div class="w-1/4">
+        <CardDashboard />
       </div>
-
+      <div class="w-1/4">
+        <CardDashboard />
+      </div>
+      <div class="w-1/4">
+        <CardDashboard />
+      </div>
     </div>
   </div>
+
 </template>
