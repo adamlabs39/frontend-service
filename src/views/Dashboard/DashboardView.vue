47609e7ad123c856c6b8eb5f9af6b913db4d0ff2
adamrasyid01 <adamrasyid01@gmail.com>
2024-08-12 09:59:22 +0700

    Date selesai

--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import CardDashboard from './CardDashboard.vue'
+import CustomDatePicker from '@/components/Base/CustomDatePicker.vue';
+
+const startDateFilter = ref<Date>(new Date())
+const endDateFilter = ref<Date>(new Date())
 
 </script>
 
@@ -13,9 +18,15 @@ import CardDashboard from './CardDashboard.vue'
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2l4-4m2 1.5L9 21m0-7h3" />
         </svg>
       </div>
-      <div class="ml-2 font-semibold text-teal-500 text-subTitle">Dashboard</div>
+      <div class="flex items-center justify-between w-full font-semibold text-teal-500 text-subTitle ">Dashboard
+        <div class="flex">
+          <CustomDatePicker v-model="startDateFilter" label="" class="w-[150px]" />
+          <PhMinus class="mt-auto mb-2 mx-[10px] text-black" />
+          <CustomDatePicker v-model="endDateFilter" :showLabel="false" class="mt-auto w-[150px]" />
+        </div>
+      </div>
     </div>
-    
+
     <div class="flex gap-2.5">
       <div class="w-1/4">
         <CardDashboard />
