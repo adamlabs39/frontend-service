695d84716b43cd4baf4844beb465013fa6eb4ac0
fahmi-nugroho <fahminugroho23@gmail.com>
2024-08-14 20:35:24 +0700

    feat: add general consent admisi flow view

--- src/views/Admisi/Layout/RegisterForm.vue
+++ src/views/Admisi/Layout/RegisterForm.vue
@@ -15,6 +15,7 @@ import CustomChip from "@/components/Base/CustomChip.vue";
 import CustomTextfield from "@/components/Base/CustomTextfield.vue";
 import CustomCheckbox from "@/components/Base/CustomCheckbox.vue";
 import CustomDialog from "@/components/Base/CustomDialog.vue";
+import { PhArrowArcRight } from "@phosphor-icons/vue";
 
 const props = defineProps({
   pageType: {
@@ -44,7 +45,9 @@ const onPaymentMethodSelect = (label: string) => {
 };
 
 const confirmSaveDialog = ref(false);
+const inputGeneralConsentDialog = ref(false);
 const generalConsentDialog = ref(false);
+const generalConsentDialogInputType = ref("create");
 
 const generalConsentType = ref(["Pasien", "Keluarga"]);
 const selectedGeneralConsent = ref("Pasien");
@@ -707,7 +710,7 @@ const isDetail = () => {
           />
           <div class="bg-adameds-300 w-[1px] my-[5px] mx-[15px]"></div>
           <CustomButton
-            @click="() => {}"
+            @click="generalConsentDialog = true"
             label="General Consent"
             class="mr-[10px]"
             backgroundColor="bg-adameds-300"
@@ -732,7 +735,16 @@ const isDetail = () => {
     </Card>
 
     <CustomDialog v-model:visible="confirmSaveDialog" width="600px">
-      <template #header>General Consent - Rawat Jalan</template>
+      <template #header>
+        General Consent -
+        {{
+          pageType == "rawat-jalan"
+            ? "Rawat Jalan"
+            : pageType == "rawat-inap"
+            ? "Rawat Inap"
+            : "IGD"
+        }}
+      </template>
       <template #body>
         <div class="mt-5">
           <div class="mb-2">
@@ -754,7 +766,9 @@ const isDetail = () => {
           textColor="text-adameds-300"
         />
         <CustomButton
-          @click="(confirmSaveDialog = false), (generalConsentDialog = true)"
+          @click="
+            (confirmSaveDialog = false), (inputGeneralConsentDialog = true)
+          "
           label="Buat"
           class=""
           backgroundColor="bg-adameds-300"
@@ -762,7 +776,7 @@ const isDetail = () => {
       </template>
     </CustomDialog>
 
-    <CustomDialog v-model:visible="generalConsentDialog" width="1000px">
+    <CustomDialog v-model:visible="inputGeneralConsentDialog" width="1000px">
       <template #header>
         <div class="flex">
           <div>General Consent</div>
@@ -793,6 +807,7 @@ const isDetail = () => {
               label="Nama Lengkap Keluarga"
               class="col-span-2"
               placeholder="Nama Lengkap Keluarga"
+              :disabled="generalConsentDialogInputType == 'detail'"
             />
             <CustomSelect
               label="Jenis Kelamin"
@@ -802,6 +817,7 @@ const isDetail = () => {
               optionValue=""
               :showFilter="false"
               :options="['Laki-laki', 'Perempuan']"
+              :disabled="generalConsentDialogInputType == 'detail'"
             />
             <CustomSelect
               label="Hubungan Dengan Pasien"
@@ -832,6 +848,7 @@ const isDetail = () => {
                 'Lainnya',
                 'Family Lain',
               ]"
+              :disabled="generalConsentDialogInputType == 'detail'"
             />
           </div>
           <CustomSelect
@@ -848,6 +865,7 @@ const isDetail = () => {
             optionValue=""
             :options="['Format 1', 'Format 2', 'Format 3']"
             prependIcon="PhMagnifyingGlass"
+            :disabled="generalConsentDialogInputType == 'detail'"
           />
           <div
             class="h-[400px] border-[1px] border-grey-200 border-dashed rounded-[10px] mt-5 flex"
@@ -885,19 +903,103 @@ const isDetail = () => {
         </div>
       </template>
       <template #footer>
-        <CustomButton
-          label="Batal"
-          outlined
-          class=""
-          borderColor="border-grey-200"
-          textColor="text-grey-300"
-        />
-        <CustomButton
-          @click="emit('goToDetail'), (generalConsentDialog = false)"
-          label="Setuju & Simpan"
-          class=""
-          backgroundColor="bg-adameds-300"
-        />
+        <div
+          v-if="generalConsentDialogInputType == 'detail'"
+          class="flex w-full"
+        >
+          <CustomButton
+            @click="emit('goToDetail'), (inputGeneralConsentDialog = false)"
+            icon="PhPrinter"
+            label="Cetak"
+            class="mr-auto"
+            backgroundColor="bg-adameds-300"
+          />
+          <CustomButton
+            @click="generalConsentDialogInputType = 'edit'"
+            label="Edit"
+            class=""
+            backgroundColor="bg-adameds-300"
+          />
+        </div>
+        <div v-else-if="generalConsentDialogInputType == 'edit'" class="flex">
+          <CustomButton
+            @click="inputGeneralConsentDialog = false"
+            label="Batal Edit"
+            outlined
+            class="mr-[10px]"
+            borderColor="border-grey-200"
+            textColor="text-grey-300"
+          />
+          <CustomButton
+            @click="emit('goToDetail'), (inputGeneralConsentDialog = false)"
+            label="Simpan Edit"
+            class=""
+            backgroundColor="bg-adameds-300"
+          />
+        </div>
+        <div v-else>
+          <CustomButton
+            @click="inputGeneralConsentDialog = false"
+            label="Batal"
+            outlined
+            class="mr-[10px]"
+            borderColor="border-grey-200"
+            textColor="text-grey-300"
+          />
+          <CustomButton
+            @click="emit('goToDetail'), (inputGeneralConsentDialog = false)"
+            label="Setuju & Simpan"
+            class=""
+            backgroundColor="bg-adameds-300"
+          />
+        </div>
+      </template>
+    </CustomDialog>
+
+    <CustomDialog v-model:visible="generalConsentDialog" width="600px">
+      <template #header>General Consent</template>
+      <template #body>
+        <div class="mt-[10px]">
+          <div v-for="data in [1, 2]">
+            <div
+              @click="
+                (generalConsentDialogInputType = 'detail'),
+                  (inputGeneralConsentDialog = true)
+              "
+              class="text-black cursor-pointer text-SM"
+            >
+              <div class="font-bold">
+                General Consent
+                {{ data == 1 ? " Umum" : "Tidak Menggunakan BPJS" }}
+              </div>
+              <div class="text-adameds-300">Rawat Jalan</div>
+              <div class="flex">
+                Tanggal
+                <PhArrowRight
+                  :size="18"
+                  class="mx-5 ml-2 mr-3 text-success-300"
+                  weight="bold"
+                />
+                2024-3-10 10:00
+              </div>
+            </div>
+            <hr class="my-[10px]" />
+          </div>
+          <div
+            class="border-[1px] border-dashed border-grey-200 h-20 rounded-[10px] flex"
+          >
+            <CustomButton
+              @click="
+                (generalConsentDialog = false),
+                  (inputGeneralConsentDialog = true)
+              "
+              icon="PhPlus"
+              label="General Consent Baru"
+              class="m-auto"
+              backgroundColor="bg-adameds-300"
+            />
+          </div>
+        </div>
       </template>
     </CustomDialog>
   </div>
