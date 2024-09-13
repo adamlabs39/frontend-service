4927f2c5998237ca7a9ffdb911cd5bdf75d19adf
fahmi-nugroho <fahminugroho23@gmail.com>
2024-09-13 23:40:05 +0700

    feat: add base layout asesment page

--- src/views/MedicalRecord/MedicalRecord.vue
+++ src/views/MedicalRecord/MedicalRecord.vue
@@ -12,6 +12,23 @@ import RMCustomSelect from "@/components/Base/RMCustomSelect.vue";
 import NoData from "@/components/section/NoData.vue";
 import PemeriksaanGigi from "@/components/RekamMedis/PemeriksaanGigi/PemeriksaanGigi.vue";
 import PemeriksaanMata from "@/components/RekamMedis/PemeriksaanMata/PemeriksaanMata.vue";
+import FormAlergi from "@/components/RekamMedis/Alergi/FormAlergi.vue";
+import Anamnesis from "@/components/RekamMedis/Anamnesis/Anamnesis.vue";
+import FormTandaVital from "@/components/RekamMedis/TandaVital/FormTandaVital.vue";
+import Antropometri from "@/components/RekamMedis/Antropometri/Antropometri.vue";
+import FormAsesmenNyeri from "@/components/RekamMedis/AsesmenNyeri/FormAsesmenNyeri.vue";
+import Kesadaran from "@/components/RekamMedis/Kesadaran/Kesadaran.vue";
+import CatatanHasilPenunjang from "@/components/RekamMedis/CatatanHasilPenunjang/CatatanHasilPenunjang.vue";
+import FormDiagnosaDokterICD10 from "@/components/RekamMedis/DiagnosaDokter/FormDiagnosaDokterICD10.vue";
+import AsuhanKeperawatan from "@/components/RekamMedis/AsuhanKeperawatan/AsuhanKeperawatan.vue";
+import FormCatatanPerawat from "@/components/RekamMedis/CatatanPerawat/FormCatatanPerawat.vue";
+import InstruksiMedis from "@/components/RekamMedis/IntruksiMedis/InstruksiMedis.vue";
+import PemeriksaanTindakan from "@/components/RekamMedis/PemeriksaanTindakan/PemeriksaanTindakan.vue";
+import FormOrderObat from "@/components/RekamMedis/OrderObat/FormOrderObat.vue";
+import FormOrderAlkes from "@/components/RekamMedis/OrderAlkes/FormOrderAlkes.vue";
+import OrderLab from "@/components/RekamMedis/OrderLab/OrderLab.vue";
+import FormOrderFisio from "@/components/RekamMedis/OrderFisio/FormOrderFisio.vue";
+import FormPersetujuanPasien from "@/components/RekamMedis/PersetujuanPasien/FormPersetujuanPasien.vue";
 
 const dialogRM = ref(false);
 const selectedTab = ref("rekam-medis");
@@ -22,6 +39,8 @@ const historyVisitDialog = ref(false);
 const assesmentList = ref([
   "Alergi",
   "Anamnesis",
+  "Anamnesis IGD",
+  "Triase",
   "Tanda Vital",
   "Antropometri",
   "Asesmen Nyeri",
@@ -399,7 +418,35 @@ defineExpose({ showDialogRM });
           </div>
 
           <div v-if="selectedTab == 'rekam-medis'" class="flex mt-5 h-3/5">
-            <NoData class="grow" title="Belum Ada Pemeriksaan" />
+            <!-- <NoData class="grow" title="Belum Ada Pemeriksaan" /> -->
+            <div class="grid grid-cols-2 grow gap-2.5">
+              <FormAlergi method="detail" />
+              <Anamnesis method="detail" />
+              <div>Anamnesis IGD</div>
+              <div>Triase</div>
+              <FormTandaVital method="detail" />
+              <Antropometri method="detail" />
+              <FormAsesmenNyeri method="detail" />
+              <Kesadaran method="detail" />
+              <PemeriksaanFisik method="detail" />
+              <PemeriksaanGigi method="detail" />
+              <PemeriksaanMata method="detail" />
+              <BurnInput method="detail" />
+              <CatatanHasilPenunjang method="detail" />
+              <FormDiagnosaDokterICD10 method="detail" />
+              <AsuhanKeperawatan method="detail" />
+              <FormCatatanPerawat method="detail" />
+              <InstruksiMedis method="detail" />
+              <FormOrderObat method="detail" />
+              <PemeriksaanTindakan method="detail" />
+              <div>Implementation</div>
+              <div>Evaluation</div>
+              <div>Reassesment</div>
+              <FormOrderAlkes method="detail" />
+              <OrderLab method="detail" />
+              <FormOrderFisio method="detail" />
+              <FormPersetujuanPasien method="detail" />
+            </div>
             <div class="flex flex-col mx-[10px]">
               <CustomButton icon="PhArrowsInLineVertical" class="mb-[10px]" />
               <CustomButton icon="PhArrowsOutLineVertical" class="" />
@@ -407,6 +454,66 @@ defineExpose({ showDialogRM });
           </div>
 
           <div v-if="selectedTab == 'asesmen'">
+            <FormAlergi
+              v-if="selectedAssesment == 'Alergi'"
+              method="form"
+              initialState="0"
+            />
+            <Anamnesis
+              v-if="selectedAssesment == 'Anamnesis'"
+              method="form"
+              initialState="0"
+            />
+            <FormTandaVital
+              v-if="selectedAssesment == 'Tanda Vital'"
+              method="form"
+              initialState="0"
+            />
+            <Antropometri
+              v-if="selectedAssesment == 'Antropometri'"
+              method="form"
+              initialState="0"
+            />
+            <FormAsesmenNyeri
+              v-if="selectedAssesment == 'Asesmen Nyeri'"
+              method="form"
+              initialState="0"
+            />
+            <Kesadaran
+              v-if="selectedAssesment == 'Kesadaran'"
+              method="form"
+              initialState="0"
+            />
+            <CatatanHasilPenunjang
+              v-if="selectedAssesment == 'Catatan Hasil Penunjang'"
+              method="form"
+              initialState="0"
+            />
+            <FormDiagnosaDokterICD10
+              v-if="selectedAssesment == 'Diagnosis Dokter'"
+              method="form"
+              initialState="0"
+            />
+            <AsuhanKeperawatan
+              v-if="selectedAssesment == 'Asuhan Keperawatan'"
+              method="form"
+              initialState="0"
+            />
+            <FormCatatanPerawat
+              v-if="selectedAssesment == 'Catatan Perawat'"
+              method="form"
+              initialState="0"
+            />
+            <InstruksiMedis
+              v-if="selectedAssesment == 'Intruksi Medis'"
+              method="form"
+              initialState="0"
+            />
+            <PemeriksaanTindakan
+              v-if="selectedAssesment == 'Pemeriksaan dan Tindakan'"
+              method="form"
+              initialState="0"
+            />
             <PemeriksaanFisik v-if="selectedAssesment == 'Pemeriksaan Fisik'" />
             <PemeriksaanGigi v-if="selectedAssesment == 'Pemeriksaan Gigi'" />
             <PemeriksaanMata v-if="selectedAssesment == 'Pemeriksaan Mata'" />
