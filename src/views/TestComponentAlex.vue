913363299ec9720f8b75f41f052ed873e6209667
Alexander W <alexanderwbw15@gmail.com>
2024-09-16 21:53:22 +0700

    fix: component Alex

--- src/views/TestComponentAlex.vue
+++ src/views/TestComponentAlex.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-import FormAlergi from "@/components/RekamMedis/Alergi/FormAlergi.vue";
-import FormAsesmenNyeri from "@/components/RekamMedis/AsesmenNyeri/FormAsesmenNyeri.vue";
-import FormTandaVital from "@/components/RekamMedis/TandaVital/FormTandaVital.vue";
-import FormCatatanPerawat from "@/components/RekamMedis/CatatanPerawat/FormCatatanPerawat.vue";
-import FormDiagnosaDokterICD10 from "@/components/RekamMedis/DiagnosaDokter/FormDiagnosaDokterICD10.vue";
-import Anamnesis from "@/components/RekamMedis/Anamnesis/Anamnesis.vue";
 
 import AnamnesisIGD from "@/components/RekamMedis/AnamnesisIGD/AnamnesisIGD.vue";
 import Triase from "@/components/RekamMedis/Triase/Triase.vue";
@@ -18,17 +12,6 @@ import FormReassesment from "@/components/Fisioterapi/Reassesment/FormReassesmen
 
 <template>
   <div class="flex flex-col h-screen gap-4 m-4 overflow-scroll">
-    <h1 class="font-bold">component Adam</h1>
-    <FormAlergi />
-    <FormTandaVital />
-    <FormAsesmenNyeri />
-    <FormCatatanPerawat />
-    <FormDiagnosaDokterICD10 />
-    <br />
-    <h1 class="font-bold">component Naya</h1>
-    <Anamnesis />
-    <br />
-    
     <h1 class="font-bold">component Alex</h1>
     <AnamnesisIGD />
     <Triase />
