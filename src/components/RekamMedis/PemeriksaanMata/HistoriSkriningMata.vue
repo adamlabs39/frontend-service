585e054e0c3525981d09033dbe2236df7d014eed
fahmi-nugroho <fahminugroho23@gmail.com>
2024-10-01 13:47:00 +0700

    feat: add Pemeriksaan Mata assesment history and compare

---
+++ src/components/RekamMedis/PemeriksaanMata/HistoriSkriningMata.vue
@@ -0,0 +1,189 @@
+<script setup lang="ts">
+import CustomAccordion from "@/components/Base/CustomAccordion.vue";
+import CustomInfoRow from "@/components/Base/CustomInfoRow.vue";
+</script>
+
+<template>
+  <CustomAccordion headerClass="bg-adameds-50" initialState="0">
+    <template #header>01 Jan 2024</template>
+    <template #content>
+      <div class="py-5 flex flex-col gap-[10px]">
+        <div class="font-semibold text-adameds-300 text-normal mb-[30px]">
+          Tajam Penglihatan menggunakan Tes Uncorrected Snelled Chart
+        </div>
+        <CustomInfoRow
+          label="Pemeriksaan Visus Kanan"
+          value="-"
+          type="vertical"
+        />
+        <hr class="border-adameds-300" />
+        <CustomInfoRow
+          label="Pemeriksaan Visus Kiri"
+          value="-"
+          type="vertical"
+        />
+        <hr class="-mx-4 border-grey-200 mt-[10px]" />
+
+        <div class="font-semibold text-adameds-300 text-normal mb-[30px]">
+          Katarak
+        </div>
+        <CustomInfoRow
+          label="Pemeriksaan Pinhole Kanan"
+          value="-"
+          type="vertical"
+        />
+        <CustomInfoRow
+          label="Pemeriksaan Segment Anterior (Pupil) Kanan*"
+          value="-"
+          type="vertical"
+        />
+        <hr class="border-adameds-300" />
+        <CustomInfoRow
+          label="Pemeriksaan Pinhole Kiri"
+          value="-"
+          type="vertical"
+        />
+        <CustomInfoRow
+          label="Pemeriksaan Segment Anterior (Pupil) Kiri*"
+          value="-"
+          type="vertical"
+        />
+        <hr class="-mx-4 border-grey-200 mt-[10px]" />
+
+        <div class="font-semibold text-adameds-300 text-normal mb-[30px]">
+          Jika Pemeriksaan Pupil hasilnya Positif, maka dilanjutkan pemeriksaan
+          dengan Oftalmoskop/Shadow test
+        </div>
+        <CustomInfoRow
+          label="Pemeriksaan Reflek Fundus Kanan"
+          value="-"
+          type="vertical"
+        />
+        <CustomInfoRow
+          label="Pemeriksaan Shadow Test Kanan"
+          value="-"
+          type="vertical"
+        />
+        <hr class="border-adameds-300" />
+        <CustomInfoRow
+          label="Pemeriksaan Reflek Fundus Kiri"
+          value="-"
+          type="vertical"
+        />
+        <CustomInfoRow
+          label="Pemeriksaan Shadow Test Kiri"
+          value="-"
+          type="vertical"
+        />
+        <hr class="-mx-4 border-grey-200 mt-[10px]" />
+
+        <div class="font-semibold text-adameds-300 text-normal mb-[30px]">
+          Kelainan Refraksi Mata Jauh
+        </div>
+        <div class="font-semibold underline text-normal">
+          Pemeriksaan Refraksi Subjektif Jauh Mata Kanan
+        </div>
+        <div class="grid grid-cols-3 gap-y-[19px]">
+          <CustomInfoRow label="Sph" value="-" type="vertical" />
+          <CustomInfoRow label="Cyl" value="-" type="vertical" />
+          <CustomInfoRow label="Axis" value="-" type="vertical" />
+        </div>
+        <div class="font-semibold underline text-normal">
+          Pemeriksaan Visus Tajam Penglihatan Jarak Jauh Mata Kanan
+        </div>
+        <div class="grid grid-cols-2">
+          <CustomInfoRow label="Nilai" value="-" type="vertical" />
+          <CustomInfoRow label="Hasil" value="-" type="vertical" />
+        </div>
+        <div class="font-semibold underline text-normal">
+          Pemeriksaan Refraksi Subjektif Jauh Mata Kiri
+        </div>
+        <div class="grid grid-cols-3 gap-y-[19px]">
+          <CustomInfoRow label="Sph" value="-" type="vertical" />
+          <CustomInfoRow label="Cyl" value="-" type="vertical" />
+          <CustomInfoRow label="Axis" value="-" type="vertical" />
+        </div>
+        <div class="font-semibold underline text-normal">
+          Pemeriksaan Visus Tajam Penglihatan Jarak Jauh Mata Kiri
+        </div>
+        <div class="grid grid-cols-2">
+          <CustomInfoRow label="Nilai" value="-" type="vertical" />
+          <CustomInfoRow label="Hasil" value="-" type="vertical" />
+        </div>
+        <hr class="-mx-4 border-grey-200 mt-[10px]" />
+
+        <div class="font-semibold text-adameds-300 text-normal mb-[30px]">
+          Jika hasil pemeriksaan adalah buta, maka dilakukan pemeriksaan
+          lanjutan
+        </div>
+        <CustomInfoRow
+          label="Pemeriksaan Lanjutan Jika Dinyatakan Buta Warna Kanan"
+          value="-"
+          type="vertical"
+        />
+        <hr class="border-adameds-300" />
+        <CustomInfoRow
+          label="Pemeriksaan Lanjutan Jika Dinyatakan Buta Warna Kiri"
+          value="-"
+          type="vertical"
+        />
+        <hr class="-mx-4 border-grey-200 mt-[10px]" />
+
+        <div class="font-semibold text-adameds-300 text-normal mb-[30px]">
+          Kelainan Refraksi Mata Dekat
+        </div>
+        <div class="font-semibold underline text-normal">
+          Pemeriksaan Refraksi Dekat Mata Kanan
+        </div>
+        <CustomInfoRow
+          label="Koreksi Lensa addisi untuk penglihatan dekat (Sph +) Kanan"
+          value="-"
+          type="vertical"
+        />
+        <CustomInfoRow
+          label="Dicurigai Glaukoma pada mata Kanan"
+          value="-"
+          type="vertical"
+        />
+        <CustomInfoRow
+          label="Dicurigai Retinopati pada Mata Kanan"
+          value="-"
+          type="vertical"
+        />
+        <div class="font-semibold underline text-normal">
+          Pemeriksaan Refraksi Dekat Mata Kiri
+        </div>
+        <CustomInfoRow
+          label="Koreksi Lensa addisi untuk penglihatan dekat (Sph +) Kiri"
+          value="-"
+          type="vertical"
+        />
+        <CustomInfoRow
+          label="Dicurigai Glaukoma pada mata Kiri"
+          value="-"
+          type="vertical"
+        />
+        <CustomInfoRow
+          label="Dicurigai Retinopati pada Mata Kiri"
+          value="-"
+          type="vertical"
+        />
+
+        <hr class="border-grey-200" />
+        <div class="flex justify-between">
+          <CustomInfoRow
+            label="Petugas Input"
+            :value="`petugas`"
+            type="vertical"
+          />
+          <CustomInfoRow
+            label="Jam Input"
+            :value="`petugas`"
+            type="vertical"
+            alignment="right"
+          />
+        </div>
+      </div>
+    </template>
+  </CustomAccordion>
+</template>
--- src/components/RekamMedis/PemeriksaanMata/TemuanLainnya.vue
+++ src/components/RekamMedis/PemeriksaanMata/TemuanLainnya.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import CustomAccordion from "@/components/Base/CustomAccordion.vue";
 import CustomInfoRow from "@/components/Base/CustomInfoRow.vue";
 import CustomTextArea from "@/components/Base/CustomTextArea.vue";
+import CustomButton from "@/components/Base/CustomButton.vue";
+import HistoriTemuanLainnya from "@/components/RekamMedis/PemeriksaanMata/HistoriTemuanLainnya.vue";
+import CustomDialog from "@/components/Base/CustomDialog.vue";
 
 const props = defineProps({
   method: {
@@ -9,6 +13,11 @@ const props = defineProps({
     default: "form",
   },
 });
+
+const compareDialog = ref(false);
+const showDialogCompare = () => {
+  compareDialog.value = true;
+};
 </script>
 
 <template>
@@ -16,6 +25,16 @@ const props = defineProps({
     <template #header>Temuan Lainnya</template>
     <template #content>
       <div class="pt-5">
+        <div v-if="method == 'form'" class="flex flex-col">
+          <CustomButton
+            @click="showDialogCompare"
+            class="!rounded-md my-[10px] ml-auto"
+            label="Mode Compare"
+            size="small"
+            icon="LayoutIcon"
+          />
+          <hr class="mb-[30px]" />
+        </div>
         <CustomTextArea
           v-if="method == 'form'"
           label="Temuan Lainnya"
@@ -23,6 +42,69 @@ const props = defineProps({
         />
         <CustomInfoRow v-else label="Temuan Lainnya" value="-" />
       </div>
+
+      <!-- Dialog compare -->
+      <CustomDialog class="" v-model:visible="compareDialog" width="80%">
+        <template #header>Antropometri</template>
+        <template #body>
+          <div class="pt-5 grid grid-cols-[1fr_min-content_1fr]">
+            <div>
+              <div class="mb-[18px] flex justify-between">
+                <div class="font-semibold text-grey-400">
+                  Riwayat Sebelumnya
+                </div>
+                <div class="flex">
+                  <CustomButton
+                    @click="() => {}"
+                    class="!rounded-md mr-[10px]"
+                    size="small"
+                    icon="PhCaretLeft"
+                  />
+                  <CustomButton
+                    @click="() => {}"
+                    class="!rounded-md"
+                    size="small"
+                    icon="PhCaretRight"
+                  />
+                </div>
+              </div>
+              <div class="grid grid-cols-[1fr_min-content_1fr]">
+                <HistoriTemuanLainnya />
+                <div class="border border-adameds-300 mx-[15px]"></div>
+                <HistoriTemuanLainnya />
+              </div>
+            </div>
+            <div class="border border-adameds-300 mx-[15px]"></div>
+            <div class="flex flex-col gap-y-5">
+              <CustomTextArea
+                label="Temuan Lainnya"
+                placeholder="Masukkan Temuan Lainnya..."
+              />
+            </div>
+          </div>
+        </template>
+        <template #footer>
+          <div class="flex items-end justify-end gap-3">
+            <CustomButton
+              v-if="method == 'form'"
+              label="Reset"
+              textColor="text-grey-300"
+              backgroundColor="bg-transparent"
+              borderColor="border-2 border-grey-200"
+            />
+            <CustomButton
+              v-if="method == 'form'"
+              label="Simpan"
+              @click="() => {}"
+            />
+            <CustomButton
+              v-if="method == 'detail'"
+              label="Edit"
+              @click="() => {}"
+            />
+          </div>
+        </template>
+      </CustomDialog>
     </template>
   </CustomAccordion>
 </template>
