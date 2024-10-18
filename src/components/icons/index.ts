ca7048a28f52fc6e9119422c6001fe8f92cc4595
Alexander W <alexanderwbw15@gmail.com>
2024-10-18 17:34:52 +0700

    feat: add antrian > apm icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -26,6 +26,12 @@ import HistoryIcon from "./HistoryIcon.vue";
 import FileImportIcon from "./FileImportIcon.vue";
 import TrashFillIcon from "./TrashFillIcon.vue";
 import HandshakeIcon from "./HandshakeIcon.vue";
+import SolarUserCheckIcon from "./SolarUserCheckIcon.vue";
+import CheckCircleIcon from "./CheckCircleIcon.vue";
+import HouseFill from "./HouseFill.vue";
+import AddPrint from "./AddPrint.vue";
+import Stethoscop from "./Stethoscop.vue";
+import PlusIcon from "./PlusIcon.vue";
 
 export default {
   install: (app: App<Element>) => {
@@ -56,5 +62,11 @@ export default {
     app.component("FileImportIcon", FileImportIcon);
     app.component("TrashFillIcon", TrashFillIcon);
     app.component("HandshakeIcon", HandshakeIcon);
+    app.component("SolarUserCheckIcon", SolarUserCheckIcon);
+    app.component("CheckCircleIcon", CheckCircleIcon);
+    app.component("HouseFill", HouseFill);
+    app.component("AddPrint", AddPrint);
+    app.component("Stethoscop", Stethoscop);
+    app.component("PlusIcon", PlusIcon);
   },
 };
