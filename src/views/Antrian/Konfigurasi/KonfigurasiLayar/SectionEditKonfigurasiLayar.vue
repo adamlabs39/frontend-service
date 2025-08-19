5a61402df71b4f4bf7aa244e2d643605d4f09191
aerochops <150325960+aerochops@users.noreply.github.com>
2025-08-19 13:42:03 +0700

    style(KonfigurasiLayar): improve readability of flash text section by adding text color

--- src/views/Antrian/Konfigurasi/KonfigurasiLayar/SectionEditKonfigurasiLayar.vue
+++ src/views/Antrian/Konfigurasi/KonfigurasiLayar/SectionEditKonfigurasiLayar.vue
@@ -360,7 +360,9 @@ watch(
                   <layout-1-list-1-panggilan />
                 </template>
               </div>
-              <div class="mt-1 rounded-tl-lg rounded-tr-lg bg-adameds-300">
+              <div
+                class="mt-1 text-white rounded-tl-lg rounded-tr-lg bg-adameds-300"
+              >
                 <Vue3Marquee>
                   <span v-for="item in flashText" :key="item" class="mx-2">{{
                     item
