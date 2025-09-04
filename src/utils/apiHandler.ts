6cc9b92ceae7bae4af2f44b755a714375422cddc
aerochops <150325960+aerochops@users.noreply.github.com>
2025-09-04 16:01:30 +0700

    feat(antrian): add api handler and store method for admisi antrian
    Add new apiAntrianAdmisiGet function to handle GET requests for admisi antrian data and implement corresponding store method with query parameter support

--- src/utils/apiHandler.ts
+++ src/utils/apiHandler.ts
@@ -780,6 +780,15 @@ const apiAntrianGetDatamaster = async (url: string, data: object) => {
     errorApiHandler(error);
   }
 };
+const apiAntrianAdmisiGet = async (url: string, data: object) => {
+  url = cekHost(import.meta.env.VITE_BASE_ANTRIAN_LOKAL, url);
+  try {
+    let response = await baseInstanceAntrian.get(url, data);
+    return response.data;
+  } catch (error) {
+    errorApiHandler(error);
+  }
+};
 export {
   apiBasePost,
   apiBaseGet,
@@ -836,4 +845,5 @@ export {
   apiAntrianPost,
   apiAntrianPut,
   apiAntrianGetDatamaster,
+  apiAntrianAdmisiGet,
 };
