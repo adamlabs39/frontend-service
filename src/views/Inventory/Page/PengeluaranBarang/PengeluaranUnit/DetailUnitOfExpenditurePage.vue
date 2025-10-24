2e05b9e390d5ad49470b3a41516511cfe7185499
bgsprayogi <prayogigres@gmail.com>
2025-10-24 09:32:37 +0700

    feat: Slicing pengeluaran unit page + detail page pengeluaran unit

--- src/views/Inventory/Page/PengeluaranBarang/PengeluaranUnit/DetailUnitOfExpenditurePage.vue
+++ src/views/Inventory/Page/PengeluaranBarang/PengeluaranUnit/DetailUnitOfExpenditurePage.vue
@@ -57,64 +57,41 @@ onMounted(() => {
 
 <template>
   <div class="flex flex-col h-full overflow-hidden">
-    <Card pt:body:class="h-full pt-0 pb-0 overflow-auto" pt:content:class="h-full overflow-hidden" class="h-full overflow-hidden overflow-y-auto">
+    <Card pt:body:class="h-full pt-0 pb-0 overflow-auto" pt:content:class="h-full overflow-hidden"
+      class="h-full overflow-hidden overflow-y-auto">
       <template #header>
         <CustomAccordion :openWithHeader="false" noBorder initialState="0">
           <template #header>
             <div class="flex justify-between w-full align-middle">
               <div class="flex">
                 <CustomButton icon="PhArrowClockwise" class="mr-5" />
-                <CustomBreadCrumb
-                  :home="{
-                    label: 'Pengeluaran Barang',
-                    home: true,
-                  }"
-                />
-                <PhCaretRight :size="25" weight="bold" class="ml-[10px] mt-[8px] text-adameds-300"/>
+                <CustomBreadCrumb :home="{
+                  label: 'Pengeluaran Barang',
+                  home: true,
+                }" />
+                <PhCaretRight :size="25" weight="bold" class="ml-[10px] mt-[8px] text-adameds-300" />
                 <div class="">
                   <p class="font-semibold text-heading text-grey-400 ml-[10px] mt-[5px]">
                     Pengeluaran Unit
                   </p>
                 </div>
-                <PhCaretRight :size="25"  weight="bold" class="ml-[10px] mt-[8px] text-grey-300"/>
+                <PhCaretRight :size="25" weight="bold" class="ml-[10px] mt-[8px] text-grey-300" />
                 <div class="ml-[10px] mt-[5px]">
-                  <CustomChip
-                    :label="props.selectedData.noPengeluaran"
-                    :showCheckedIcon="false"
-                    borderColor="border-adameds-300"
-                    bgColor="bg-adameds-300"
-                    textColor="text-white"
-                  />
+                  <CustomChip :label="props.selectedData.noPengeluaran" :showCheckedIcon="false"
+                    borderColor="border-adameds-300" bgColor="bg-adameds-300" textColor="text-white" />
                 </div>
                 <div v-if="props.selectedData.status == 'cancel'" class="ml-[10px] mt-[5px]">
-                  <CustomChip
-                    label="DIBATALKAN"
-                    :showCheckedIcon="false"
-                    borderColor="border-danger-300"
-                    bgColor="bg-danger-300"
-                    textColor="text-white"
-                  />
+                  <CustomChip label="DIBATALKAN" :showCheckedIcon="false" borderColor="border-danger-300"
+                    bgColor="bg-danger-300" textColor="text-white" />
                 </div>
                 <div v-if="props.selectedData.status == 'verifikasi'" class="ml-[10px] mt-[5px]">
-                  <CustomChip
-                    label="DIVERIFIKASI"
-                    :showCheckedIcon="false"
-                    borderColor="border-info-300"
-                    bgColor="bg-info-300"
-                    textColor="text-white"
-                  />
+                  <CustomChip label="DIVERIFIKASI" :showCheckedIcon="false" borderColor="border-info-300"
+                    bgColor="bg-info-300" textColor="text-white" />
                 </div>
               </div>
               <div class="flex">
-                <CustomButton
-                  @click="emit('back')"
-                  icon="PhCaretLeft"
-                  label="Kembali"
-                  class="mr-[10px]"
-                  outlined
-                  borderColor="border-adameds-300"
-                  textColor="text-adameds-300"
-                />
+                <CustomButton @click="emit('back')" icon="PhCaretLeft" label="Kembali" class="mr-[10px]" outlined
+                  borderColor="border-adameds-300" textColor="text-adameds-300" />
               </div>
             </div>
           </template>
@@ -127,7 +104,7 @@ onMounted(() => {
               </div>
               <!-- Tgl. Permintaan -->
               <div>
-                <p class="text-xs font-bold underline underline-offset-2">Lokasi Penerima</p>
+                <p class="text-xs font-bold underline underline-offset-2">Tgl. Permintaan</p>
                 <p>{{ epochToDate(DetailPayload.tanggalPengeluaran, "date") }}</p>
               </div>
               <!-- Kategori Item -->
@@ -156,14 +133,14 @@ onMounted(() => {
                 <p class="text-xs font-bold underline underline-offset-2">
                   Tujuan Pengeluaran
                 </p>
-                <p>{{ DetailPayload.jenisItem }}</p>
+                <p>{{ DetailPayload.lokasiStokAkhir || '-' }}</p>
               </div>
               <!-- Jenis Pemusnahan -->
               <div v-if="DetailPayload.jenisPengeluaran == 'pemusnahan barang'">
                 <p class="text-xs font-bold underline underline-offset-2">
                   Jenis Pemusnahan
                 </p>
-                <p>{{ DetailPayload.jenisItem }}</p>
+                <p>{{ DetailPayload.jenisPemusnahan || '-' }}</p>
               </div>
               <!-- Petugas Pengeluaran -->
               <div>
@@ -177,61 +154,63 @@ onMounted(() => {
                 <p class="text-xs font-bold underline underline-offset-2">
                   Catatan
                 </p>
-                <p>{{ DetailPayload.catatan }}</p>
+                <p>{{ DetailPayload.catatan || '-' }}</p>
               </div>
             </div>
             <hr class="mt-5 border-1 border-grey-200" />
             <div class="mt-[20px] h-[260px]">
-              <DataTable
-                :value="DetailPayload.items"
-                scrollable
-                scrollHeight="flex"
-                :pt="{ headerRow: 'text-SM' }"
-              >
-                <!-- No -->
-                <Column headerClass="bg-adameds-50">
+              <DataTable :value="DetailPayload.items" scrollable scrollHeight="flex" :pt="{ headerRow: 'text-SM' }">
+                <Column headerClass="bg-adameds-50 w-[50px]" class="w-[50px]">
                   <template #header>
-                    <div class="w-full font-semibold">No.</div>
+                    <div class="font-semibold">No.</div>
                   </template>
                   <template #body="slotProps">
-                    <div class="text-sm">{{ slotProps.index + 1 }}</div>
-                  </template>
-                </Column>
-                <!-- Nama Item -->
-                <Column header="Nama Item" headerClass="bg-adameds-50">
-                  <template #body="slotProps">
-                    <div class="text-sm">{{ slotProps.data.nama }}</div>
+                    <div class="text-sm text-center">{{ slotProps.index + 1 }}</div>
                   </template>
                 </Column>
-                <!-- Exp. Date -->
-                <Column header="Exp. Date" headerClass="bg-adameds-50">
+                <Column header="Nama Item" headerClass="bg-adameds-50 font-semibold">
                   <template #body="slotProps">
-                    <div class="text-sm">
+                    <div class="text-sm">{{ slotProps.data.name || '-' }}</div>
+                  </template>
+                </Column>
+                <Column header="Exp. Date" headerClass="bg-adameds-50 font-semibold w-[120px]" class="w-[120px]">
+                  <template #body="slotProps">
+                    <div class="text-sm ">
                       {{ formatDate(slotProps.data.expDate) }}
                     </div>
                   </template>
                 </Column>
-                <!-- Min. Stok -->
-                <Column field="stok" header="Min. Stok" headerClass="bg-adameds-50 font-semibold text-SM"></Column>
-                <!-- Stok -->
-                <Column field="qty" header="Stok" headerClass="bg-adameds-50 font-semibold text-SM"></Column>
-                <!-- Pengeluaran -->
-                <Column field="pengeluaran" header="Pengeluaran" headerClass="bg-adameds-50 font-semibold text-SM"></Column>
-                <!-- Satuan/Isi -->
-                <Column field="satuan" header="Satuan/Isi" headerClass="bg-adameds-50 font-semibold text-SM"></Column>
-                <!-- Harga Dasar -->
-                <Column header="Harga Dasar" headerClass="bg-adameds-50">
+                <Column header="Min. Stok" headerClass="bg-adameds-50 font-semibold w-[100px]" class="w-[100px]">
                   <template #body="slotProps">
-                    <div class="text-sm">
-                      {{ formatPrice(slotProps.data.hargaSatuan) }}
+                    <div class="text-sm text-center">{{ slotProps.data.qty ?? '-' }}</div>
+                  </template>
+                </Column>
+                <Column header="Stok" headerClass="bg-adameds-50 font-semibold w-[100px]" class="w-[100px]">
+                  <template #body="slotProps">
+                    <div class="text-sm ">{{ slotProps.data.stok ?? '-' }}</div>
+                  </template>
+                </Column>
+                <Column header="Pengeluaran" headerClass="bg-adameds-50 font-semibold w-[100px]" class="w-[100px]">
+                  <template #body="slotProps">
+                    <div class="text-sm text-center">{{ slotProps.data.pengeluaran ?? '-' }}</div>
+                  </template>
+                </Column>
+                <Column header="Satuan/Isi" headerClass="bg-adameds-50 font-semibold w-[120px]" class="w-[120px]">
+                  <template #body="slotProps">
+                    <div class="text-sm">{{ slotProps.data.satuan || '-' }}</div>
+                  </template>
+                </Column>
+                <Column header="Harga Dasar" headerClass="bg-adameds-50 font-semibold w-[150px]" class="w-[150px]">
+                  <template #body="slotProps">
+                    <div class="text-sm ">
+                      {{ formatPrice(slotProps.data.hargaSatuan || 0) }}
                     </div>
                   </template>
                 </Column>
-                <!-- Total -->
-                <Column header="Total" headerClass="bg-adameds-50">
+                <Column header="Total" headerClass="bg-adameds-50 font-semibold w-[150px]" class="w-[150px]">
                   <template #body="slotProps">
-                    <div class="text-sm">
-                      {{ formatPrice(slotProps.data.totalHarga) }}
+                    <div class="text-sm ">
+                      {{ formatPrice(slotProps.data.total || 0) }}
                     </div>
                   </template>
                 </Column>
@@ -239,42 +218,34 @@ onMounted(() => {
             </div>
           </template>
           <template #footer>
-              <div class="flex">
-                <div>
-                    <CustomButton background-color="bg-adameds-300 rounded-lg">
-                        <div class="flex items-center gap-2">
-                            <PhPrinter :size="18" colorc="#ffffff" weight="fill" />
-                            <div class="text-sm">Cetak</div>
-                        </div>
-                    </CustomButton>
-                </div>
-                <div class="ml-[30px]">
-                  <p class="font-bold underline underline-offset-2">
-                    Total Item
-                  </p>
-                  <p>{{ DetailPayload.totalItem }}</p>
-                </div>
-                <div class="ml-[30px]">
-                  <p class="font-bold underline underline-offset-2">
-                    Grand Total
-                  </p>
-                  <p>{{ DetailPayload.totalHarga }}</p> 
-                </div>
+            <div class="flex">
+              <div>
+                <CustomButton background-color="bg-adameds-300 rounded-lg">
+                  <div class="flex items-center gap-2">
+                    <PhPrinter :size="18" colorc="#ffffff" weight="fill" />
+                    <div class="text-sm">Cetak</div>
+                  </div>
+                </CustomButton>
               </div>
+              <div class="ml-[30px]">
+                <p class="font-bold underline underline-offset-2">
+                  Total Item
+                </p>
+                <p>{{ DetailPayload.totalItem }}</p>
+              </div>
+              <div class="ml-[30px]">
+                <p class="font-bold underline underline-offset-2">
+                  Grand Total
+                </p>
+                <p>{{ DetailPayload.totalHarga }}</p>
+              </div>
+            </div>
           </template>
           <template #collapseIcon>
-            <CustomButton
-              icon="PhCaretUp"
-              backgroundColor="bg-adameds-75"
-              textColor="text-adameds-300"
-            />
+            <CustomButton icon="PhCaretUp" backgroundColor="bg-adameds-75" textColor="text-adameds-300" />
           </template>
           <template #expandIcon>
-            <CustomButton
-              icon="PhCaretDown"
-              backgroundColor="bg-adameds-75"
-              textColor="text-adameds-300"
-            />
+            <CustomButton icon="PhCaretDown" backgroundColor="bg-adameds-75" textColor="text-adameds-300" />
           </template>
         </CustomAccordion>
       </template>
